2710ac642042cca77f61dd8cf508875d96cd545f
Milan Rawat <milanrawat086@gmail.com>
2022-05-01 11:20:19 +0530

    loader for my account

--- screens/accountScreens/Account.js
+++ screens/accountScreens/Account.js
@@ -8,6 +8,7 @@ import {
   TouchableNativeFeedback,
   Image,
   Alert,
+  Dimensions,
 } from 'react-native';
 import EncryptedStorage from 'react-native-encrypted-storage';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -18,6 +19,7 @@ import GlobalContext from '../../context/GlobalContext';
 const Account = ({ navigation }) => {
   const [userData, setUserData] = React.useState({});
   const [isLoggedIn, setIsLoggedIn] = React.useContext(GlobalContext);
+  const [isLoaded, setIsLoaded] = React.useState(false);
 
   const retrieveUserSession = async () => {
     try {
@@ -43,6 +45,7 @@ const Account = ({ navigation }) => {
       });
       const response = JSON.parse(await res.text());
       setUserData(response.me);
+      setIsLoaded(true);
     } catch (err) {
       console.log(err);
     }
@@ -80,128 +83,143 @@ const Account = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.screen}>
-      <View style={styles.headerBar}>
-        <Ionicons
-          onPress={() => navigation.goBack()}
-          name="arrow-back"
-          size={20}
-          color="white"
-        />
-        <TouchableOpacity
-          activeOpacity={1}
-          style={styles.keywordContainer}
-          onPress={() => navigation.goBack()}>
-          <Text style={styles.keyword}>My Account</Text>
-          <Ionicons
-            onPress={() => navigation.navigate('SearchScreen')}
-            name="search"
-            size={20}
-            color="white"
-          />
-        </TouchableOpacity>
-        <Ionicons
-          onPress={() => navigation.navigate('CartScreen')}
-          name="cart"
-          size={20}
-          color="white"
-        />
-      </View>
-      <ScrollView>
-        <View style={styles.screenHeader}>
-          <View style={styles.profileContainer}>
-            <Image
-              style={styles.profileImage}
-              source={{
-                uri:
-                  userData && userData.profilePicture
-                    ? userData.profilePicture
-                    : 'https://i.stack.imgur.com/l60Hf.png',
-              }}
-            />
-          </View>
-          <Text style={styles.name}>
-            {userData && userData.fullName ? userData.fullName : 'User'}
-          </Text>
-          <View style={styles.nameContainer}>
-            <Text>{userData && userData.email ? userData.email : ''}</Text>
-            <Ionicons
-              style={styles.isVerified}
-              name={
-                userData.emailVerified ? 'checkmark-circle' : 'close-circle'
-              }
-              size={20}
-              color={userData.emailVerified ? '#5DE23C' : '#F72F35'}
-            />
-          </View>
+    <>
+      {!isLoaded && (
+        <View
+          style={{
+            flex: 1,
+            height: Dimensions.get('window').height - 250,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text style={{ color: 'black', fontSize: 18 }}>Loading...</Text>
         </View>
+      )}
+      {isLoaded && (
+        <View style={styles.screen}>
+          <View style={styles.headerBar}>
+            <Ionicons
+              onPress={() => navigation.goBack()}
+              name="arrow-back"
+              size={20}
+              color="white"
+            />
+            <TouchableOpacity
+              activeOpacity={1}
+              style={styles.keywordContainer}
+              onPress={() => navigation.goBack()}>
+              <Text style={styles.keyword}>My Account</Text>
+              <Ionicons
+                onPress={() => navigation.navigate('SearchScreen')}
+                name="search"
+                size={20}
+                color="white"
+              />
+            </TouchableOpacity>
+            <Ionicons
+              onPress={() => navigation.navigate('CartScreen')}
+              name="cart"
+              size={20}
+              color="white"
+            />
+          </View>
+          <ScrollView>
+            <View style={styles.screenHeader}>
+              <View style={styles.profileContainer}>
+                <Image
+                  style={styles.profileImage}
+                  source={{
+                    uri:
+                      userData && userData.profilePicture
+                        ? userData.profilePicture
+                        : 'https://i.stack.imgur.com/l60Hf.png',
+                  }}
+                />
+              </View>
+              <Text style={styles.name}>
+                {userData && userData.fullName ? userData.fullName : 'User'}
+              </Text>
+              <View style={styles.nameContainer}>
+                <Text>{userData && userData.email ? userData.email : ''}</Text>
+                <Ionicons
+                  style={styles.isVerified}
+                  name={
+                    userData.emailVerified ? 'checkmark-circle' : 'close-circle'
+                  }
+                  size={20}
+                  color={userData.emailVerified ? '#5DE23C' : '#F72F35'}
+                />
+              </View>
+            </View>
 
-        <View style={styles.otherContainer}>
-          <OptionCard
-            data={{ title: 'Shopkart Plus', btnName: 'Get Shopkart Plus' }}
-          />
-          <OptionCard
-            data={{ title: 'My Orders', btnName: 'View All Orders' }}
-          />
-          <OptionCard
-            data={{ title: 'My Wishlist', btnName: 'View All wishlist' }}
-          />
-          <OptionCard data={{ title: 'My Cart', btnName: 'View Cart' }} />
-          <OptionCard
-            data={{ title: 'My Card & wallet', btnName: 'View Details' }}
-          />
-          <OptionCard
-            data={{ title: 'My Reviews', btnName: 'View Your Reviews' }}
-          />
+            <View style={styles.otherContainer}>
+              <OptionCard
+                data={{ title: 'Shopkart Plus', btnName: 'Get Shopkart Plus' }}
+              />
+              <OptionCard
+                data={{ title: 'My Orders', btnName: 'View All Orders' }}
+              />
+              <OptionCard
+                data={{ title: 'My Wishlist', btnName: 'View All wishlist' }}
+              />
+              <OptionCard data={{ title: 'My Cart', btnName: 'View Cart' }} />
+              <OptionCard
+                data={{ title: 'My Card & wallet', btnName: 'View Details' }}
+              />
+              <OptionCard
+                data={{ title: 'My Reviews', btnName: 'View Your Reviews' }}
+              />
+            </View>
+            <View style={styles.lastOptions}>
+              <Text style={styles.option}>
+                <Ionicons
+                  style={styles.icon}
+                  onPress={() => {}}
+                  name="notifications"
+                  size={20}
+                  color="black"
+                />
+                {'  '}
+                Notifications
+              </Text>
+              <Text style={styles.option}>
+                <Ionicons
+                  style={styles.icon}
+                  onPress={() => {}}
+                  name="settings-outline"
+                  size={20}
+                  color="black"
+                />
+                {'  '}
+                Account Settings
+              </Text>
+              <Text style={styles.option} onPress={() => onLogout()}>
+                <Ionicons
+                  style={styles.icon}
+                  onPress={() => {}}
+                  name="exit-outline"
+                  size={20}
+                  color="black"
+                />
+                {'  '}
+                Logout of this app
+              </Text>
+              <Text style={styles.option}>
+                <Ionicons
+                  style={styles.icon}
+                  onPress={() => {}}
+                  name="exit-outline"
+                  size={20}
+                  color="black"
+                />
+                {'  '}
+                Logout of all devices
+              </Text>
+            </View>
+          </ScrollView>
         </View>
-        <View style={styles.lastOptions}>
-          <Text style={styles.option}>
-            <Ionicons
-              style={styles.icon}
-              onPress={() => {}}
-              name="notifications"
-              size={20}
-              color="black"
-            />
-            {'  '}
-            Notifications
-          </Text>
-          <Text style={styles.option}>
-            <Ionicons
-              style={styles.icon}
-              onPress={() => {}}
-              name="settings-outline"
-              size={20}
-              color="black"
-            />
-            {'  '}
-            Account Settings
-          </Text>
-          <Text style={styles.option} onPress={() => onLogout()}>
-            <Ionicons
-              style={styles.icon}
-              onPress={() => {}}
-              name="exit-outline"
-              size={20}
-              color="black"
-            />
-            {'  '}
-            Logout of this app
-          </Text>
-          <Text style={styles.option}>
-            <Ionicons
-              style={styles.icon}
-              onPress={() => {}}
-              name="exit-outline"
-              size={20}
-              color="black"
-            />
-            {'  '}
-            Logout of all devices
-          </Text>
-        </View>
-      </ScrollView>
-    </View>
+      )}
+    </>
   );
 };
 
